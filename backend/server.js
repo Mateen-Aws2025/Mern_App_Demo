b801377b5e7955c9b186d3b9d07dec1e1d8f7a41
Mateen Sayyed Sardar <mateen.aws2025@gmail.com>
2026-04-17 04:41:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,4 +43,10 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.get('/', (req, res) => {
+  res.send('Backend running 🚀');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
